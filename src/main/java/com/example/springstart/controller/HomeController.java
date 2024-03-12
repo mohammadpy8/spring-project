85d4cb415b28d7c4368c18dc9113670ea6d017d4
mohammadpy8 <mohammadpy8@gmail.com>
2024-03-12 13:26:14 +0330

    pathVariable and requestParam

--- src/main/java/com/example/springstart/controller/HomeController.java
+++ src/main/java/com/example/springstart/controller/HomeController.java
@@ -1,9 +1,7 @@
 package com.example.springstart.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/home")
@@ -11,12 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/index")
-    public String home(){
+    public String home() {
         return "HomeController";
     }
+
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         return "test";
     }
-}
 
+    @GetMapping("/user")
+    public String[] user() {
+        return new String[]{"name", "full_name"};
+    }
+
+    @GetMapping("/post/{number1}/{number2}")
+    public String post(@PathVariable Long number1, @PathVariable Long number2) {
+        return String.valueOf(number1 + number2);
+    }
+    @GetMapping("/new-post")
+    public String newPost(@RequestParam Long number1, @RequestParam Long number2) {
+        return String.valueOf(number1 + number2);
+    }
+}
