b0b4306a51cf80e01a5d5f7ce1809854f840c50a
mohammadpy8 <mohammadpy8@gmail.com>
2024-03-13 11:12:26 +0330

    CREATE TABLE

--- src/main/java/com/example/springstart/model/Person1.java
+++ src/main/java/com/example/springstart/model/Person1.java
@@ -1,10 +1,24 @@
 package com.example.springstart.model;
+
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Table(name = "SEYF")
 public class Person1 {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @SequenceGenerator(name = "table")
+    @Column(name = "first_table")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Getter
+    @Setter
     private Long id;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private String lastname;
+
 }
