8ccfd444d47cdb51959d3ac93e693cb199ac2cd3
mohammadpy8 <mohammadpy8@gmail.com>
2024-03-13 14:33:30 +0330

    CREATE PUT INTO DB

--- src/main/java/com/example/springstart/controller/PersonController.java
+++ src/main/java/com/example/springstart/controller/PersonController.java
@@ -4,10 +4,7 @@ import com.example.springstart.model.Person1;
 import com.example.springstart.service.Person1Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/person")
@@ -17,8 +14,13 @@ public class PersonController {
     private Person1Service person1Service;
 
     @PostMapping
-    public ResponseEntity<Person1> save(@RequestBody Person1 person1){
+    public ResponseEntity<Person1> save(@RequestBody Person1 person1) {
         Person1 save = person1Service.save(person1);
         return ResponseEntity.ok(save);
+    }
+
+    @PutMapping
+    public ResponseEntity<Person1> update(@RequestBody Person1 person1) {
+        return ResponseEntity.ok(person1Service.update(person1));
     }
 }
--- src/main/java/com/example/springstart/service/Person1Service.java
+++ src/main/java/com/example/springstart/service/Person1Service.java
@@ -10,7 +10,17 @@ public class Person1Service {
 
     @Autowired
     private Person1Repository person1Repository;
+
     public Person1 save(Person1 person1) {
         return person1Repository.save(person1);
+    }
+
+    public Person1 update(Person1 person1) {
+        Person1 updatePerson = person1Repository.findById(person1.getId())
+                .orElseThrow(() -> new RuntimeException("not found"));
+        updatePerson.setName(person1.getName());
+        updatePerson.setLastname(person1.getLastname());
+        Person1 saveUpdate = person1Repository.save(updatePerson);
+        return saveUpdate;
     }
 }
